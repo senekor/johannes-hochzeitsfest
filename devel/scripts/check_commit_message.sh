2141001fa8e766cc4f901e16b6e7a08ca9411f03
Remo Senekowitsch <remo@buenzli.dev>
2025-02-13 16:01:04 +0100

    ci: improve event triggers 🥳
    - Don't run CI checks on every push anymore. This makes it less annoying
      to push "work in progress" commits.
    - Do run CI checks on every push to the main branch.
    - Run CI checks on pull requests.
    
    Documentation of the `pull_request` event trigger for reference:
    https://docs.github.com/en/actions/writing-workflows/choosing-when-your-workflow-runs/events-that-trigger-workflows#pull_request

--- devel/scripts/check_commit_message.sh
+++ devel/scripts/check_commit_message.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# commit to check: first arg or HEAD
+commit_to_check="${1:-$(git rev-parse HEAD)}"
+
+if test -n "${CI+x}" ; then
+    # GitHub CI may not have this ref fetched by default
+    git fetch origin "$commit_to_check"
+fi
+
 echo "Checking commit message for 🥳 emoji..."
-if ! git log -1 --pretty=%B | grep -qE "(:partying_face:|🥳)"
+if ! git log -1 --pretty=%B "$commit_to_check" | grep -qE "(:partying_face:|🥳)"
 then
     echo "Please be more joyful !! 🥳"
     exit 1
 fi
 
 echo "Checking commit message subject length..."
-if (( "$(git log -1 --pretty=%s | wc -c)" > 50 ))
+if (( "$(git log -1 --pretty=%s "$commit_to_check" | wc -c)" > 50 ))
 then
     echo "Please avoid lengthy commit message subjects!"
     echo "https://cbea.ms/git-commit/#limit-50"
